9cc82cb10bc3bd3bdf24b3065c55b790507f2238
Leonard Wieloch <leonsoshi@gmail.com>
2025-08-09 14:25:44 +0200

    Casting walls to float points vector

--- include/CubeMeshJsonParser.h
+++ include/CubeMeshJsonParser.h
@@ -1,8 +1,11 @@
 #pragma once
 
 #include "interfaces/IReadableJson.h"
+#include "types/gl_types.h"
 
 #include <vector>
+
+namespace {
 
 constexpr int maxPoints = 4;
 constexpr size_t maxVertices = 5;
@@ -11,17 +14,17 @@ struct Wall {
   float pointArray[maxPoints][maxVertices];
 };
 
-class CubeMeshJsonParser final : public IReadableJson {
-private:
-  std::vector<Wall> walls;
+}
 
+class CubeMeshJsonParser final : public IReadableJson {
 public:
-  CubeMeshJsonParser();
+  CubeMeshJsonParser() = default;
   virtual ~CubeMeshJsonParser() = default;
 
-private:
-  void parseJsonToVector();
+  std::vector<gl::GLfloat> getVerticesFromVectorOfWalls() const;
 
+private:
   json getDataFromJsonFile(const std::string &filename) const override;
-  std::vector<Wall> deriveWalls() const;
+
+  std::vector<Wall> parseJsonToVectorOfWalls() const;
 };
--- src/CubeMeshJsonParser.cpp
+++ src/CubeMeshJsonParser.cpp
@@ -8,100 +8,112 @@
 
 #include "patterns/cube/inc/cube_data_pattern.h"
 
-void setWallArrayValue(Wall &wall, int idPoint, int idVertex, float value);
-
-CubeMeshJsonParser::CubeMeshJsonParser() { parseJsonToVector(); }
-
-void CubeMeshJsonParser::parseJsonToVector() {
-  const auto data = getDataFromJsonFile(ASSETS_PATH "/cube/json/mesh.json");
-  const auto meshVertices = data["Mesh"]["vertices"];
-
-  Wall frontWall, backWall, leftWall, rightWall, bottomWall, topWall;
-
-  for (const auto &wallName : patterns::cube::wallOrderPattern) {
-
-    auto itWall = meshVertices.find(wallName);
-    if (itWall == meshVertices.end())
-      continue;
-
-    const auto &wallData = itWall.value();
-
-    // std::cout << wallName << ": " << "\n";
-
-    int idPoint = 0;
-
-    for (const auto &pointName : patterns::cube::pointOrderPattern) {
-      auto itPoint = wallData.find(pointName);
-      if (itPoint == wallData.end())
-        continue;
-
-      const auto &pointData = itPoint.value();
-
-      // std::cout << pointName << ": ";
-
-      int idVertex = 0;
-
-      for (const auto &coordKey : patterns::cube::vertexOrderPattern) {
-        auto itCoord = pointData.find(coordKey);
-        if (itCoord != pointData.end()) {
-          if (std::string_view(wallName) == "front-wall") {
-            setWallArrayValue(frontWall, idPoint, idVertex,
-                              itCoord.value().get<float>());
-            idVertex++;
-          }
-          if (std::string_view(wallName) == "back-wall") {
-            setWallArrayValue(backWall, idPoint, idVertex,
-                              itCoord.value().get<float>());
-            idVertex++;
-          }
-          if (std::string_view(wallName) == "left-wall") {
-            setWallArrayValue(leftWall, idPoint, idVertex,
-                              itCoord.value().get<float>());
-            idVertex++;
-          }
-          if (std::string_view(wallName) == "right-wall") {
-            setWallArrayValue(rightWall, idPoint, idVertex,
-                              itCoord.value().get<float>());
-            idVertex++;
-          }
-          if (std::string_view(wallName) == "bottom-wall") {
-            setWallArrayValue(bottomWall, idPoint, idVertex,
-                              itCoord.value().get<float>());
-            idVertex++;
-          }
-          if (std::string_view(wallName) == "top-wall") {
-            setWallArrayValue(topWall, idPoint, idVertex,
-                              itCoord.value().get<float>());
-            idVertex++;
-          }
-        }
-      }
-      idPoint++;
-    }
-  }
-
-  walls.push_back(frontWall);
-  walls.push_back(backWall);
-  walls.push_back(leftWall);
-  walls.push_back(rightWall);
-  walls.push_back(bottomWall);
-}
+namespace {
 
 void setWallArrayValue(Wall &wall, int idPoint, int idVertex, float value) {
-  wall.pointArray[idPoint][idVertex] = value;
+    wall.pointArray[idPoint][idVertex] = value;
+}
+
 }
 
 json CubeMeshJsonParser::getDataFromJsonFile(
     const std::string &filename) const {
-  std::ifstream file{filename};
-  if (!file.is_open()) {
-    std::cerr << "Not found JSON file: " << filename << "\n";
-    return EXIT_FAILURE;
-  }
+    std::ifstream file{filename};
+    if (!file.is_open()) {
+        std::cerr << "Not found JSON file: " << filename << "\n";
+        return EXIT_FAILURE;
+    }
 
-  json data;
-  file >> data;
-  return data;
+    json data;
+    file >> data;
+    return data;
 }
 
-std::vector<Wall> CubeMeshJsonParser::deriveWalls() const { return walls; }
+std::vector<Wall> CubeMeshJsonParser::parseJsonToVectorOfWalls() const {
+    const auto data = getDataFromJsonFile(ASSETS_PATH "/cube/json/mesh.json");
+    const auto meshVertices = data["Mesh"]["vertices"];
+
+    std::vector<Wall> walls;
+    Wall frontWall, backWall, leftWall, rightWall, bottomWall, topWall;
+
+    for (const auto &wallName : patterns::cube::wallOrderPattern) {
+
+        auto itWall = meshVertices.find(wallName);
+        if (itWall == meshVertices.end())
+            continue;
+
+        const auto &wallData = itWall.value();
+
+        int idPoint = 0;
+
+        for (const auto &pointName : patterns::cube::pointOrderPattern) {
+            auto itPoint = wallData.find(pointName);
+            if (itPoint == wallData.end())
+                continue;
+
+            const auto &pointData = itPoint.value();
+
+            int idVertex = 0;
+
+            for (const auto &coordKey : patterns::cube::vertexOrderPattern) {
+                auto itCoord = pointData.find(coordKey);
+                if (itCoord != pointData.end()) {
+                    if (std::string_view(wallName) == "front-wall") {
+                        setWallArrayValue(frontWall, idPoint, idVertex,
+                                          itCoord.value().get<float>());
+                        idVertex++;
+                    }
+                    if (std::string_view(wallName) == "back-wall") {
+                        setWallArrayValue(backWall, idPoint, idVertex,
+                                          itCoord.value().get<float>());
+                        idVertex++;
+                    }
+                    if (std::string_view(wallName) == "left-wall") {
+                        setWallArrayValue(leftWall, idPoint, idVertex,
+                                          itCoord.value().get<float>());
+                        idVertex++;
+                    }
+                    if (std::string_view(wallName) == "right-wall") {
+                        setWallArrayValue(rightWall, idPoint, idVertex,
+                                          itCoord.value().get<float>());
+                        idVertex++;
+                    }
+                    if (std::string_view(wallName) == "bottom-wall") {
+                        setWallArrayValue(bottomWall, idPoint, idVertex,
+                                          itCoord.value().get<float>());
+                        idVertex++;
+                    }
+                    if (std::string_view(wallName) == "top-wall") {
+                        setWallArrayValue(topWall, idPoint, idVertex,
+                                          itCoord.value().get<float>());
+                        idVertex++;
+                    }
+                }
+            }
+            idPoint++;
+        }
+    }
+
+    walls.push_back(frontWall);
+    walls.push_back(backWall);
+    walls.push_back(leftWall);
+    walls.push_back(rightWall);
+    walls.push_back(bottomWall);
+    walls.push_back(topWall);
+
+    return walls;
+}
+
+std::vector<gl::GLfloat> CubeMeshJsonParser::getVerticesFromVectorOfWalls() const {
+    auto vecWalls = parseJsonToVectorOfWalls();
+    std::vector<gl::GLfloat> vertices;
+    vertices.reserve(vecWalls.size() * maxPoints * maxVertices);
+
+    for(const auto& wall : vecWalls){
+        const gl::GLfloat* begin = &wall.pointArray[0][0];
+        const gl::GLfloat* end = begin + (maxPoints * maxVertices);
+
+        vertices.insert(vertices.end(), begin, end);
+    }
+    return vertices;
+}
